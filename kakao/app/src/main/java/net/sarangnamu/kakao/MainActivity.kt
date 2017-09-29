bcdd0bb3c1709728b8cb0021e116a4be867f74d8
aucd29 <aucd29@hanwha.com>
2017-09-29 17:18:50 +0900

    - 4번 푸는 중

--- kakao/app/src/main/java/net/sarangnamu/kakao/MainActivity.kt
+++ kakao/app/src/main/java/net/sarangnamu/kakao/MainActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import java.lang.Math.pow
+import java.util.*
 
 // https://github.com/kakao/kakao.github.io/blob/5656b06546a63c6b33d9b5204d919b5e90b5a7bb/_posts/2017-09-27-kakao-blind-recruitment-round-1.md
 
@@ -12,9 +13,125 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        q3()
+        q4()
+//        q3()
 //        q2()
 //        q1()
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    //
+    // 오전 9시부터 총 n회(0 ~ 10) t분(0 ~ 60) 간격으로 역에 도착
+    //
+    // 최대로 탑승할 수 있는 승객 수 m (0 ~ 45)
+    // timetable 의 최소 길이는 1 최대는 2000
+    // 도착 시각은 00:01 에서 23:59 사이
+    //
+    // 사무실에 갈 수 있는 제일 늦은 도착 시작 출력
+    //
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    fun q4() {
+//        Q4().process()
+        Q4().apply {
+            n = 2
+            t = 10
+            m = 2
+            timetable = arrayListOf("09:10", "09:09", "08:00")
+        }.process()
+    }
+
+    data class Q4Time (
+        val date: String,
+        val timestamp: Long
+    )
+
+    class Q4 {
+        val YEAR = 2017
+        val MONTH = 9
+        val DAY = 29
+        val HOUR = 9
+        val MIN = 0
+        val SEC = 0
+
+        var n = 1   // 회수
+        var t = 1   // 간격 (분)
+        var m = 5   // 탑승 인원
+        var timetable = arrayListOf("08:00", "08:01", "08:02", "08:03")
+
+        fun parseCrewTime(): ArrayList<ArrayList<Q4Time>> {
+            val crewTimeList = ArrayList<ArrayList<Q4Time>>()
+
+            timetable.sort()
+            val it = timetable.listIterator()
+            while (it.hasNext()) {
+                var i = 0
+                val list = ArrayList<Q4Time>()
+
+                while (i++ < m) {
+                    if (!it.hasNext()) {
+                        break
+                    }
+
+                    val time      = it.next()
+                    val converted = convertCrewTime(time)
+
+                    Log.i("q4", "CREW [$time] = $converted")
+                    list.add(Q4Time(time, converted))
+                }
+
+                crewTimeList.add(list)
+            }
+
+            return crewTimeList
+        }
+
+        fun convertCrewTime(time: String): Long {
+            val tm   = time.split(":")
+            val hour = tm.get(0).toInt()
+            val min  = tm.get(1).toInt()
+
+            return Date(YEAR, MONTH, DAY, hour, min, SEC).time
+        }
+
+        fun process() {
+            var busStartTime = Date(YEAR, MONTH, DAY, HOUR, MIN, SEC).time
+            Log.i("q4", "BUS START = $busStartTime")
+
+            val crewTimeList = parseCrewTime()
+            // arrayListOf("09:10", "09:09", "08:00")
+            var i = 0
+            var lastTime:Long = 0
+            while (i < n) {
+                var it = crewTimeList.get(i).listIterator()
+                while (it.hasNext()) {
+                    val q4time = it.next()
+                    if (busStartTime <= q4time.timestamp) {
+                        Log.i("q4", "TAKE ${q4time.date}")
+                        lastTime = q4time.timestamp - 60
+                        break;
+                    }
+                }
+
+                if (n > 1) {
+                    // next bus time
+                    busStartTime += 60 * t
+                }
+
+                ++i
+            }
+
+            if (lastTime > busStartTime) {
+                lastTime = busStartTime
+            }
+
+            trace(busStartTime)
+        }
+
+        fun trace(time: Long) {
+            val date = Date(time)
+            Log.e("q4", "RESULT: " + String.format("%02d:%02d", date.hours, date.minutes))
+        }
     }
 
     ////////////////////////////////////////////////////////////////////////////////////
@@ -24,6 +141,7 @@ class MainActivity : AppCompatActivity() {
     // - cache (0 ~ 30)
     // - city name
     //   최대 100,000, eng only, maxlength 20,
+    //   // 특수 문자가 들어온 경우 예외처리를 어떻게 할지에 대한 내용이 없네?
     //
     ////////////////////////////////////////////////////////////////////////////////////
 
