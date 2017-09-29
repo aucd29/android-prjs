bcdf1cc0658ccd4c565accce10ac779e89b1c8ce
aucd29 <aucd29@hanwha.com>
2017-09-29 14:22:21 +0900

    - 2번 완료

--- kakao/app/src/main/java/net/sarangnamu/kakao/MainActivity.kt
+++ kakao/app/src/main/java/net/sarangnamu/kakao/MainActivity.kt
@@ -23,69 +23,111 @@ class MainActivity : AppCompatActivity() {
     ////////////////////////////////////////////////////////////////////////////////////
 
     fun q2() {
-        Q2().apply {
-            point = "1S2D*3T"
+        Q2().apply { dartPoint = "1S2D*3T" }.process()
+        Q2().apply { dartPoint = "1D2S#10S" }.process()
+        Q2().apply { dartPoint = "1D2S0T" }.process()
+        Q2().apply { dartPoint = "1S*2T*3S" }.process()
+        Q2().apply { dartPoint = "1D#2S*3S" }.process()
+        Q2().apply { dartPoint = "1T2D3D#" }.process()
+        Q2().apply { dartPoint = "1D2S3T*" }.process()
+    }
+
+    class Q2Result {
+        var num: Int = 0
+        var operator: Char = ' '
+        var option: Char = ' '
+        var nextRes: Q2Result? = null
+
+        fun number(value: Int) {
+            if (value < 0) {
+                num = 0
+            } else if (value > 10) {
+                num = 10
+            } else {
+                num = value
+            }
+        }
+
+        fun option(opt: Char) {
+            option = opt
+        }
+
+        fun cal(): Int {
+            var powVal = 1.0    // S
+            when (operator) {
+                'D' -> powVal = 2.0
+                'T' -> powVal = 3.0
+            }
+
+            num = pow(num.toDouble(), powVal).toInt()
+
+            when (option) {
+                '*' -> num *= 2
+                '#' -> num *= -1
+            }
+
+            nextRes?.let {
+                if ('*' == it.option) {
+                    num *= 2
+                }
+            }
+
+            return num
         }
     }
 
     class Q2 {
-        var point: String = ""
-        var pos: Int = 0
-        var temp: Int = 0
-        var result: Int = 0
+        var dartPoint: String = ""
+        var index: Int = 0
+        var totalResult: Int = 0
 
         fun process() {
-            var res = 0
+            val listData = ArrayList<Q2Result>()
             (0..2).forEach {
-                res += cal()
+                val result = cal()
+                if (listData.size > 0) {
+                    listData.last().nextRes = result
+                }
+
+                listData.add(result)
             }
 
-            result = res
+            listData.forEach {
+                totalResult += it.cal()
+            }
 
             trace()
         }
 
-        fun cal(): Int {
-            var k = point.get(pos + 1)
+        fun cal(): Q2Result {
             var value: Int
-            var start = pos
+            val start = index
+            val result = Q2Result()
 
-            ++pos
-            if (k.isDigit()) {
-                value = point.substring(start, ++pos).toInt()
+            ++index
+            if (dartPoint.get(index).isDigit()) {
+                value = dartPoint.substring(start, ++index).toInt()
             } else {
-                value = point.substring(start, pos).toInt()
+                value = dartPoint.substring(start, index).toInt()
             }
 
-            if (value < 0) {
-                value = 0
-            }
+            result.number(value)
+            result.operator = dartPoint.get(index)
 
-            if (value > 10) {
-                value = 10
-            }
-
-            val op = point.get(pos++)
-            when (op) {
-                'S' -> value = (pow(value.toDouble(), 1.0) * 2).toInt()
-                'D' -> value = (pow(value.toDouble(), 2.0)).toInt()
-                'T' -> value = (pow(value.toDouble(), 3.0)).toInt()
-            }
-
-            if (point.length >= pos) {
-                // special
-                val speical = point.get(pos++)
-                when (op) {
-                    '*' -> value *= 2
-                    '#' -> value *= -1
+            try {
+                result.option(dartPoint.get(++index))
+                if (result.option == '*' || result.option == '#') {
+                    ++index
                 }
+            } catch (ignore: Exception) {
+
             }
 
-            return value
+            return result
         }
 
         fun trace() {
-            Log.e("q2", "RES $result")
+            Log.e("[q2]", "RES ($dartPoint) = $totalResult")
         }
     }
 
@@ -98,21 +140,21 @@ class MainActivity : AppCompatActivity() {
     fun q1() {
         Q1().apply {
             n = 5
-            arr1 = listOf(9, 20, 28, 18, 11)
-            arr2 = listOf(30, 1, 21, 17, 28)
+            arr1 = arrayListOf(9, 20, 28, 18, 11)
+            arr2 = arrayListOf(30, 1, 21, 17, 28)
         }.process()
 
         Q1().apply {
             n = 6
-            arr1 = listOf(46, 33, 33 ,22, 31, 50)
-            arr2 = listOf(27 ,56, 19, 14, 14, 10)
+            arr1 = arrayListOf(46, 33, 33 ,22, 31, 50)
+            arr2 = arrayListOf(27 ,56, 19, 14, 14, 10)
         }.process()
     }
 
     class Q1 {
         var n: Int = 5
-        var arr1: List<Int> = ArrayList()
-        var arr2: List<Int> = ArrayList()
+        var arr1: ArrayList<Int> = ArrayList()
+        var arr2: ArrayList<Int> = ArrayList()
         val result: ArrayList<String> = ArrayList()
 
         fun process() {
@@ -140,19 +182,20 @@ class MainActivity : AppCompatActivity() {
             trace()
         }
 
-        private fun filter(data: List<Int>) : List<Int> {
+        private fun filter(data: ArrayList<Int>) : ArrayList<Int> {
             // https://www.quora.com/What-is-the-next-term-of-this-sequence-1-3-7-15-31-63-_
             val filter  = (pow(2.0, n.toDouble()) - 1).toInt()
-            val newData = ArrayList<Int>()
+            val it = data.listIterator()
+            while (it.hasNext()) {
+                it.set(it.next() and filter)
+            }
 
-            (0..n - 1).forEach { i -> newData += data.get(i) and filter }
-
-            return newData
+            return data
         }
 
         private fun trace() {
-            result.forEach { res -> Log.e("Q1", res) }
-            Log.e("Q1", "--")
+            result.forEach { res -> Log.e("[q1]", res) }
+            Log.e("[q1]", "--")
         }
     }
 }
